c8a81ce872917b402753476a71ee47a8119b8ac3
cheese <cheese.zhang@consultant.volvo.com>
2024-07-04 13:45:30 +0800

    web component

--- blocks/person/pserson.js
+++ blocks/person/pserson.js
@@ -1,3 +1,8 @@
 export default function decorate(block) {
-  block.innerHTML = '<person-info host="https://publish-p110498-e1334763.adobeaemcloud.com" query-param-value="John Doe"></person-info>';
+  const [personWrapper] = block.children;
+
+  const personinfo = document.createElement('person-info');
+  personinfo.setAttribute('host', 'https://publish-p110498-e1334763.adobeaemcloud.com');
+  personinfo.setAttribute('query-param-value', 'John Doe');
+  personWrapper.replaceChildren(personinfo);
 }
